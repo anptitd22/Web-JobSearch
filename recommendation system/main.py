8113a3bdd8697ca2ec2392f5a4d7d85041de750f
anptitd22 <tu123098b.email@gmail.com>
2025-04-29 14:19:39 +0700

    update 29/4

--- recommendation system/main.py
+++ recommendation system/main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from typing import Optional
 import re
 
 # Thông tin kết nối
@@ -21,6 +22,13 @@ try:
     query = "SELECT * FROM jobs where jobs.is_active = 1"
 
     app = FastAPI()
+
+
+    class UserInput(BaseModel):
+        currentJob: Optional[str] = None
+        note: Optional[str] = None
+        currentJobFunction: Optional[int] = None
+        address: Optional[str] = None
 
     # Model dữ liệu user từ Java gửi sang
     class JobInput(BaseModel):
@@ -62,6 +70,47 @@ try:
             ans.append(get_name(sortedSimilarJob[i][0]))
 
         return ans
+
+
+    @app.post("/recommend/user")
+    def recommend_jobs(user: UserInput):
+        # Tạo cột "combineFeatures" cho từng job từ DB
+        df_jobs['combineFeatures'] = df_jobs.apply(
+            lambda row: f"{row['name']} {row['description']} {row['job_function_id']} {row['job_locations']}", axis=1
+        )
+
+        user_input_parts = []
+
+        if user.currentJob is not None:
+            user_input_parts.append(user.currentJob)
+
+        if user.note is not None:
+            user_input_parts.append(user.note)
+
+        if user.currentJobFunction is not None:
+            user_input_parts.append(str(user.currentJobFunction))
+
+        if user.address is not None:
+            user_input_parts.append(str(user.address))
+
+        user_input = " ".join(user_input_parts)
+
+        # Gộp tất cả (job + user) lại để vector hóa
+        all_text = df_jobs['combineFeatures'].tolist() + [user_input]
+
+        # Vector hóa và tính độ tương đồng cosine
+        tf = TfidfVectorizer()
+        tfMatrix = tf.fit_transform(all_text)
+
+        # Chỉ tính cosine giữa user và từng job
+        cosine_similarities = cosine_similarity(tfMatrix[-1], tfMatrix[:-1]).flatten()
+        sorted_indices = cosine_similarities.argsort()[::-1]
+
+        # Lấy top 5 jobId được recommend
+        top_n = 5
+        top_job_ids = df_jobs.iloc[sorted_indices[:top_n]]['id'].tolist()
+
+        return top_job_ids
 
 except Exception as e:
     print(f"Lỗi khi truy vấn: {e}")
